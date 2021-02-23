10484e7db8336e5e0b6734491725852c31fc0a74
Santiago Archila <santiago@allbirds.com>
2021-02-23 00:47:16 -0800

    update search_images_ddg to match fastbook repo

--- fastbook/__init__.py
+++ fastbook/__init__.py
@@ -50,24 +50,31 @@ def search_images_bing(key, term, min_sz=128, max_images=150):
     response.raise_for_status()
     return L(response.json()['value'])
 
-def search_images_ddg(term, max_images=200):
-    "Search for `term` with DuckDuckGo and return a unique urls of about `max_images` images"
-    assert max_images<1000
-    url = 'https://duckduckgo.com/'
-    res = urlread(url,data={'q':term}).decode()
-    searchObj = re.search(r'vqd=([\d-]+)\&', res)
-    assert searchObj
-    requestUrl = url + 'i.js'
-    params = dict(l='us-en', o='json', q=term, vqd=searchObj.group(1), f=',,,', p='1', v7exp='a')
-    urls,data = set(),{'next':1}
-    while len(urls)<max_images and 'next' in data:
-        try:
-            data = urljson(requestUrl,data=params)
-            urls.update(L(data['results']).itemgot('image'))
-            requestUrl = url + data['next']
-        except (URLError,HTTPError): pass
-        time.sleep(0.2)
-    return L(urls)
+def search_images_ddg(key,max_n=200):
+     """Search for 'key' with DuckDuckGo and return a unique urls of 'max_n' images
+        (Adopted from https://github.com/deepanprabhu/duckduckgo-images-api)
+     """
+     url        = 'https://duckduckgo.com/'
+     params     = {'q':key}
+     res        = requests.post(url,data=params)
+     searchObj  = re.search(r'vqd=([\d-]+)\&',res.text)
+     if not searchObj: print('Token Parsing Failed !'); return
+     requestUrl = url + 'i.js'
+     headers    = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'}
+     params     = (('l','us-en'),('o','json'),('q',key),('vqd',searchObj.group(1)),('f',',,,'),('p','1'),('v7exp','a'))
+     urls       = []
+     while True:
+         try:
+             res  = requests.get(requestUrl,headers=headers,params=params)
+             data = json.loads(res.text)
+             for obj in data['results']:
+                 urls.append(obj['image'])
+                 max_n = max_n - 1
+                 if max_n < 1: return L(set(urls))     # dedupe
+             if 'next' not in data: return L(set(urls))
+             requestUrl = url + data['next']
+         except:
+             pass
 
 def plot_function(f, tx=None, ty=None, title=None, min=-2, max=2, figsize=(6,4)):
     x = torch.linspace(min,max)
